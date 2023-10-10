672c4b75c1364a22d1a797cbe2264b44593becb2
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-10-10 12:36:49 +0200

    Keep distance

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x00ff00)
 basic.forever(function () {
-	
+    if (cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters) < 8) {
+        cuteBot.motors(-30, -30)
+    } else if (cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters) < 12) {
+        cuteBot.motors(0, 0)
+    } else {
+        cuteBot.motors(30, 30)
+    }
 })
